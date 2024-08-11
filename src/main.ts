fc946d18b57be8b74ced53f71f87619bce8fae82
Amir Maghami <Maghami.A1987@gmail.com>
2024-08-11 09:22:26 +0330

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,66 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+import "./style.css";
+import {
+  AxesHelper,
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from "three";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// Size
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// Canvas
+const canvas = document.querySelector("#app")!;
+
+// Scene
+const scene = new Scene();
+
+// Controls
+// new OrbitControls()
+
+// Box
+const boxGeometry = new BoxGeometry(1, 1, 1);
+const boxMaterial = new MeshBasicMaterial({ color: 0xffffff });
+const box = new Mesh(boxGeometry, boxMaterial);
+
+// box.position.x = 1;
+box.position.set(0, 0, 0);
+scene.add(box);
+
+// Scale
+// box.scale.set(1,1,.1)
+box.rotation.x = 1;
+// box.quaternion.y = 2;
+
+// box.position.normalize();
+
+
+// Axes Helper 
+const axesHelper = new AxesHelper(1);
+scene.add(axesHelper)
+
+
+
+
+
+
+// Camera
+const camera = new PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+camera.position.x = 1;
+camera.position.y = 1;
+scene.add(camera);
+
+// Renderer
+const renderer = new WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
