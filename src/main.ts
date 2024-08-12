1c5ad5c3381d2ca553ca04f64c7d69fb296b505f
Amir Maghami <Maghami.A1987@gmail.com>
2024-08-12 15:28:42 +0330

    :sparkles: add gsap and animation

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { renderer } from "./objects/renderer";
 import { home } from "./objects/home";
 import { line } from "./objects/line";
 import { axesHelper } from "./objects/axesHelper";
-
+import gsap from "gsap";
 // Scene
 const scene = new Scene();
 scene.add(home);
@@ -17,15 +17,18 @@ renderer.render(scene, camera);
 
 new OrbitControls(camera, renderer.domElement);
 
+gsap.to(home.position, { x: 3 });
+gsap.to(home.position, { x: -3 });
 const clock = new Clock();
+
 const animate = () => {
   const elapsedTime = clock.getElapsedTime();
   console.log(elapsedTime);
   renderer.render(scene, camera);
   camera.position.y = Math.sin(elapsedTime) * 2;
   camera.position.x = Math.cos(elapsedTime);
-  camera.position.z = (Math.cos(elapsedTime) * Math.sin(elapsedTime)) * 2;
-  camera.lookAt(home.position)
+  camera.position.z = Math.cos(elapsedTime) * Math.sin(elapsedTime) * 2;
+  camera.lookAt(home.position);
   // home.position.z = Math.cos(elapsedTime);
 
   requestAnimationFrame(animate);
