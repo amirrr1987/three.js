92bebf0bf05d959db6bd44f2081a75287dc5ac3d
Amir Maghami <Maghami.A1987@gmail.com>
2024-08-18 15:21:56 +0330

    update to vue

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,14 @@
-import "./style.css";
-import { OrbitControls } from "three/examples/jsm/Addons.js";
-import { Clock, Scene } from "three";
-import { camera } from "./objects/camera";
-import { renderer } from "./objects/renderer";
-import { home } from "./objects/home";
-import { line } from "./objects/line";
-import { axesHelper } from "./objects/axesHelper";
-import gsap from "gsap";
-// Scene
-const scene = new Scene();
-scene.add(home);
-scene.add(line);
-scene.add(axesHelper);
+import './assets/main.css'
 
-renderer.render(scene, camera);
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-new OrbitControls(camera, renderer.domElement);
+import App from './App.vue'
+import router from './router'
 
-gsap.to(home.position, { x: 3 });
-gsap.to(home.position, { x: -3 });
-const clock = new Clock();
+const app = createApp(App)
 
-const animate = () => {
-  const elapsedTime = clock.getElapsedTime();
-  console.log(elapsedTime);
-  renderer.render(scene, camera);
-  camera.position.y = Math.sin(elapsedTime) * 2;
-  camera.position.x = Math.cos(elapsedTime);
-  camera.position.z = Math.cos(elapsedTime) * Math.sin(elapsedTime) * 2;
-  camera.lookAt(home.position);
-  // home.position.z = Math.cos(elapsedTime);
+app.use(createPinia())
+app.use(router)
 
-  requestAnimationFrame(animate);
-};
-animate();
+app.mount('#app')
--- src/objects/renderer.ts
+++
@@ -1,8 +0,0 @@
-import { WebGLRenderer } from "three";
-
-const canvas = document.querySelector("#app")!;
-
-export const renderer = new WebGLRenderer({
-  canvas: canvas,
-});
-renderer.setSize(window.innerWidth, window.innerHeight);
